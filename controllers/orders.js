c597dd367b6457a3a84c79ed2f40b8d8e6cb0b43
Veron Ibishi <veron@gjirafa.com>
2023-02-01 21:08:34 +0100

    Fixes

--- controllers/orders.js
+++ controllers/orders.js
@@ -3,10 +3,14 @@ const Orders = require('../models/ordersModel')
 exports.add =  async (req, res) => { 
     try { 
         const order = new Orders({
-            'space-id': req.body.id,
-            'order-duration': req.body.duration,
-            'order-price': req.body.price,
-            'special-privileges': req.body.specials
+            'space-id': req.body['space-id'],
+            'person-name': req.body['person-name'],
+            'person-email': req.body['person-email'],
+            'person-phone': req.body['person-phone'],
+            'order-duration': req.body['order-duration'],
+            'order-price': req.body['order-price'],
+            'order-privilege': req.body['order-privilege'],
+            'ordered-at': Date.now()
             
         })
         await order.save()
--- models/ordersModel.js
+++ models/ordersModel.js
@@ -3,24 +3,40 @@ const mongoose = require('../database/mongo')
 const Orders = new mongoose.Schema({ 
     'space-id': { 
         type: String,
-        required: true,
-    },
-    'order-duration': {
-        type: {
-            startAt: Date,
-            endAt: Date
-        },
-        required: true,
-    },
-    'order-price': {
+        required: true
+    }, 
+    'person-name': {
         type: String,
         required: true,
     },
-
-    'special-privileges': { 
-        type: Boolean,
+    'person-email': {
+        type: String,
         required: true,
     },
+    'person-phone': {
+        type: String,
+        required: true
+    },
+    'order-duration': { 
+        type: {
+            startAt: Date,
+            endAt: Date,
+            completed: Boolean,
+        },
+        required: true
+    },
+    'order-price': {
+        type: Number,
+        required: true
+    },
+    'order-privilege': {
+        type: Object,
+        required: false,
+    },
+    'ordered-at': { 
+        type: Date,
+        required: true,
+    }
 })
 
 module.exports = mongoose.model('orders', Orders)
